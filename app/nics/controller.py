39ac832ed3cbfedca7047518961be3b830a8d16b
ibarretorey <ibarretorey@gmail.com>
2019-11-11 15:36:45 -0300

    add launch playbook logic

--- app/nics/controller.py
+++ app/nics/controller.py
@@ -4,10 +4,11 @@ from flask.wrappers import Response
 from app.api_response import ApiResponse
 #async dependencies
 from app.utils.async_action import async_action
-from app.utils.awx import awx_fetch
+from app.utils.awx import awx_fetch, awx_post
 import asyncio
 import aiohttp
 import json
+import os
 
 api_description = """
 Representación de los switches de la empresa.
@@ -15,15 +16,8 @@ Representación de los switches de la empresa.
 
 api = Namespace('Nics', description=api_description)
 
-@api.route("/")
-# @api.response(400, 'Bad Request', interfaces.error_response_model)
-# @api.doc(responses={
-#     401: 'Unauthorized',
-#     403: 'Forbidden',
-#     500: 'Internal server error',
-#     502: 'Bad Gateway',
-#     503: 'Service Unavailable',
-# })
+@api.route("/<string:switch>")
+@api.param("id", "Identificador único del Switch")
 class InterfaceResource(Resource):
     """
     Interface Resource
@@ -33,14 +27,48 @@ class InterfaceResource(Resource):
     # @api.response(200, 'Lista de Interfaces', interfaces.many_response_model)
 
     @async_action
-    async def get(self):
+    async def get(self, switch: str):
         """
         Devuelve la lista de Interfaces
         """
-        await asyncio.sleep(5)
-        contador = 0
-        result = await awx_fetch('/api/v2/hosts')
-        return { "total": contador, "result": result }
+        # List jobs templates
+        # '/api/v2/job_templates/'
+        try:
+            result = await awx_fetch('/api/v2/job_templates/')
+            templates =  result["results"]
+            if (os.environ.get('ENV') == 'prod'):
+                templateList = list(filter(lambda x: x["name"] == "prod-show-interfaces-information", templates))
+                if (len(templateList) > 0):
+                    template = templateList[0]
+            else:
+                templateList = list(filter(lambda x: x["name"] == "test-show-interfaces-information", templates))
+                if (len(templateList) > 0):
+                    template = templateList[0]
+            
+            if (template != None):
+                launch_result = await awx_post('/api/v2/job_templates/' + str(template["id"]) + '/launch/',
+                { 
+                    "limit": switch
+                })
+                job_id = launch_result["job"]
+            else:
+                return ApiResponse({ "Error": 'The template ' + os.environ.get('ENV')  + '-show-interfaces-information not found'}, 400)
+            
+            for i in range(15):
+                job_status_result = await  awx_fetch('/api/v2/jobs/' + str(job_id) + '/')
+                if (job_status_result["status"] == "failed"):
+                    return ApiResponse({ "Error": "Playbook execution error" }, 400)
+                if (job_status_result["status"] == "successful"):
+                    return ApiResponse({ "job_id": job_id, "job_status": job_status_result["status"]})
+                # elif (job_status_result["status"] != "waiting" and job_status_result["status"] != "running"):
+                #     return ApiResponse({ "Error": "Job status unrecognized" }, 400)
+                await asyncio.sleep(2)
+            return ApiResponse({ "Error": "Playbook execution timeout error" }, 400)
+        except Exception as e:
+            return ApiResponse({ "Error": str(e) }, 400)
+            
+
+        
 
 #     @api.expect(interfaces.create_model)
 #     @api.response(200, 'Nuevo Switch', interfaces.single_response_model)
