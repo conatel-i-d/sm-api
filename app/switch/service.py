12a376cac46cc613b13c49a6cf3fbdaad913b1d8
ibarretorey <ibarretorey@gmail.com>
2020-04-08 11:10:43 -0300

    add fetch from prime

--- app/switch/service.py
+++ app/switch/service.py
@@ -19,9 +19,9 @@ class SwitchService:
         switches_from_prime = []
         ids_sw_in_db =  list(map(lambda x: int(x[0]), db.session.query(Switch.id).all()))
         try:
-            # prime_data = await prime_fetch('/webacs/api/v4/data/Devices.json?.full=true')
-            with open(os.path.join(pathlib.Path(__file__).parent.absolute(), 'prime_devices_full.json')) as json_file:
-                prime_data = json.load(json_file)
+            prime_data = await prime_fetch('/webacs/api/v4/data/Devices.json?.full=true')
+            # with open(os.path.join(pathlib.Path(__file__).parent.absolute(), 'prime_devices_full.json')) as json_file:
+            #     prime_data = json.load(json_file)
             switches = prime_data['queryResponse']['entity']
             for switch in switches:
                 switch_data = switch["devicesDTO"]
