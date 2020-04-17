80b3e5abd5986f2316c6e62645bd862f9fa01018
ibarretorey <ibarretorey@gmail.com>
2020-04-17 09:01:52 -0300

    add update by name or id

--- app/switch/service.py
+++ app/switch/service.py
@@ -17,15 +17,13 @@ class SwitchService:
     @staticmethod
     async def get_all() -> List[Switch]:
         switches_from_prime = []
-        ids_sw_in_db =  list(map(lambda x: int(x[0]), db.session.query(Switch.id).all()))
+        sw_names_in_db =  list(map(lambda x: int(x[0]), db.session.query(Switch.name).all()))
         try:
             prime_data = await prime_fetch('/webacs/api/v4/data/Devices.json?.full=true&.maxResults=300&.firstResult=0')
-            # with open(os.path.join(pathlib.Path(__file__).parent.absolute(), 'prime_devices_full.json')) as json_file:
-            #     prime_data = json.load(json_file)
             switches = prime_data['queryResponse']['entity']
             for switch in switches:
                 switch_data = switch["devicesDTO"]
-                if not (int(switch_data["deviceId"]) in ids_sw_in_db):
+                if not (int(switch_data["name"]) in sw_names_in_db):
                     SwitchService.create({
                         "id": int(switch_data["deviceId"]),
                         "name": switch_data["deviceName"],
@@ -37,13 +35,16 @@ class SwitchService:
                         "is_visible": True
                         })
                 else:
-                    SwitchService.update(int(switch_data["deviceId"]),{
-                        "name": switch_data["deviceName"],
-                        "description": "software_type: {0}, software_version: {1}".format(switch_data.get("softwareType","unknown"),switch_data.get("softwareVersion","unknown")),
-                        "model": switch_data["deviceType"], 
-                        "ip": switch_data["ipAddress"],
-                        "ansible_user": encode(os.getenv("PRIME_SWITCHES_SSH_USER")),
-                        "ansible_ssh_pass": encode(os.getenv("PRIME_SWITCHES_SSH_PASS"))
+                    db.session.query(Switch).filter(
+                        Switch.name == switch_data["name"] or Switch.id == int(switch_data["deviceId"])).update(
+                        {
+                            "id": int(switch_data["deviceId"]),
+                            "name": switch_data["deviceName"],
+                            "description": "software_type: {0}, software_version: {1}".format(switch_data.get("softwareType","unknown"),switch_data.get("softwareVersion","unknown")),
+                            "model": switch_data["deviceType"], 
+                            "ip": switch_data["ipAddress"],
+                            "ansible_user": encode(os.getenv("PRIME_SWITCHES_SSH_USER")),
+                            "ansible_ssh_pass": encode(os.getenv("PRIME_SWITCHES_SSH_PASS"))
                         })
         except Exception as err:
             print("Can't connect with prime to list switches, error: ", err, file=sys.stderr)
